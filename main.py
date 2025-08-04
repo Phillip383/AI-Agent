1ad09a71851da1c3db856e12462373c1c070a9d6
Phillip Wood <Phillipwood2023@outlook.com>
2025-08-04 09:55:36 -0400

    Added calcultor from course.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,16 +12,24 @@ client = genai.Client(api_key=api_key)
 model = 'gemini-2.0-flash-001'
 
 
+
 def main():
     response = ""
     # Command Line prompt
     contents = sys.argv[1:]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=contents[0])])
+    ]
+    response = client.models.generate_content(model=model, contents=messages)
     
-    response = client.models.generate_content(model=model, contents=contents)
-    print(response.text)
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
-
+    
+    if contents.__contains__("--verbose"):
+        print("\n")
+        print("User prompt: " + response.text)
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
